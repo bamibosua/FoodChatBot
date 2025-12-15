ef22811311a4371d8b3be0f14ebb165e58c6a64f
minhhang192 <24127033@student.hcmus.edu.vn>
2025-12-15 15:27:56 +0700

    update translator

--- Translator/state.py
+++ Translator/state.py
@@ -1,13 +1,33 @@
-# state.py
-_current_lang = None
+from deep_translator import GoogleTranslator
+from Translator.utils import is_number_plus_k
+from Translator.state import get_lang, set_lang
+from NLPModule.ai.languageDetector import ai_language_detector
 
+def detect_language(text: str) -> str:
+    """
+    Detect language of the input.
+    """
+    return ai_language_detector(text)
 
-def set_lang(lang: str):
-    """Cập nhật ngôn ngữ người dùng."""
-    global _current_lang
-    _current_lang = lang
+def get_original_language(text: str) -> str:
+    return detect_language_safe(text)
 
+def translate_text(text: str, dest_lang: str) -> str:
+    """
+    Translate text to destination language.
+    """
 
-def get_lang() -> str:
-    """Lấy ngôn ngữ hiện tại; fallback = None."""
-    return _current_lang
+    if not text or not text.strip():
+        return text
+    return GoogleTranslator(source='auto', target=dest_lang).translate(text)
+    
+def detect_language_safe(text: str) -> str:
+    """
+    Nếu input là số hoặc số+k/K → không detect → giữ hiện trạng.
+    """
+    if is_number_plus_k(text) or text.isdigit():
+        return get_lang()
+    
+    detected = detect_language(text)
+    set_lang(detected)
+    return detected
